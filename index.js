6d36eb5b3e7cc9ddd1e1b1a39f35b68fa339e63e
MASHRABBOY.@GMAIL.COM <gulomovmashrabboy51@gmail.com>
2023-11-23 14:46:52 +0500

    added new project

---
+++ index.js
@@ -0,0 +1,45 @@
+// function toogle (params) {
+//     let element = document.getElementById("togle")
+//     element.classList.toggle("dark")
+//  }
+
+// function add(a, b) {
+//     return a + b
+// }
+// console.log(add(100,50));
+
+
+// function plus(son) {
+
+//     for(let i = 1;i <= son;i++){
+//        if(i % 3 == 0 && i % 5 == 0){
+//           console.log(`${i}:`);
+//        }
+//                                   else if(i % 3 == 0){
+//           console.log(`${i} 3 ga bolinadi`);
+//        }
+//                            else if(i % 5 == 0){
+//           console.log(`${i} 5 ga bo'linadi`);
+//        }
+//                                        else{
+//           console.log(`${i}  boshqa son`);
+//        }
+//     }
+//     }
+//     plus(15)
+    
+
+
+
+let piyoda = "3 km/h"
+let velosepet = "20 km/h"
+let avtomobil = "70 km/h"
+let samaliot = "800 km/h"
+
+function hrakat(params) {
+   if (condition) {
+      
+   } else {
+      
+   }
+}
